0afb7e6b024261fb5fb57ba9523bd5d250090542
KI-Anik <iamkhairul101@gmail.com>
2025-01-26 22:18:10 +0600

    implement delete functionality in CoffeeCard component with confirmation dialog

--- client-side/src/components/CoffeeCard.jsx
+++ client-side/src/components/CoffeeCard.jsx
@@ -1,9 +1,41 @@
 import { AiOutlineDelete } from "react-icons/ai";
 import { FaEye } from "react-icons/fa";
 import { CiEdit } from "react-icons/ci";
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
-    const { name, quantity, supplier, taste, category, photo } = coffee
+    const { _id, name, quantity, supplier, taste, category, photo } = coffee
+
+    const handleDelete = id => {
+        console.log(id)
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffee/${id}`,{
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount) {
+
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className="card card-side bg-base-100 shadow-sm">
             <figure>
@@ -20,7 +52,7 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="join join-vertical space-y-2">
                         <button className="btn join-item text-2xl"> <FaEye /> </button>
                         <button className="btn join-item text-2xl"><CiEdit /></button>
-                        <button className="btn join-item text-2xl"> <AiOutlineDelete /> </button>
+                        <button onClick={() => handleDelete(_id)} className="btn join-item text-2xl bg-orange-500"> <AiOutlineDelete /> </button>
                     </div>
                 </div>
             </div>
